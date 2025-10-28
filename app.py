93778c41d1cf5887d2f23dc2195f260b03713d16
Posijour <168458508+Posijour@users.noreply.github.com>
2025-10-28 17:12:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -152,10 +152,16 @@ def cluster_worker():
 threading.Thread(target=cluster_worker, daemon=True).start()
 
 @app.route("/")
+def root():
+    return "OK", 200
+
+@app.route("/health", methods=["GET"])
 def health():
     return "OK", 200
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "8080"))
     app.run(host="0.0.0.0", port=port)
+
+
 
